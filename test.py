869bbebaf757560a10730663f41b4a8d7c915d4f
juncyan <hjq.r@outlook.com>
2024-01-18 16:46:42 +0800

    test

--- test.py
+++ test.py
@@ -1,230 +1,266 @@
-from functools import partial
-from typing import Iterable, Tuple, Union
-
 import torch
-import torch.nn.functional as f
-from torch import Tensor, nn
-from torch.fft import irfftn, rfftn
-from math import ceil, floor
+import torchvision.transforms as tfs
+import os
+import cv2
+from PIL import Image
+import random
+import numpy as np
+from torch.utils import data
+import pandas as pd
 
 
-def complex_matmul(a: Tensor, b: Tensor, groups: int = 1) -> Tensor:
-    """Multiplies two complex-valued tensors."""
-    # Scalar matrix multiplication of two tensors, over only the first channel
-    # dimensions. Dimensions 3 and higher will have the same shape after multiplication.
-    # We also allow for "grouped" multiplications, where multiple sections of channels
-    # are multiplied independently of one another (required for group convolutions).
-    a = a.view(a.size(0), groups, -1, *a.shape[2:])
-    b = b.view(groups, -1, *b.shape[1:])
-
-    a = torch.movedim(a, 2, a.dim() - 1).unsqueeze(-2)
-    b = torch.movedim(b, (1, 2), (b.dim() - 1, b.dim() - 2))
-
-    # complex value matrix multiplication
-    real = a.real @ b.real - a.imag @ b.imag
-    imag = a.imag @ b.real + a.real @ b.imag
-    real = torch.movedim(real, real.dim() - 1, 2).squeeze(-1)
-    imag = torch.movedim(imag, imag.dim() - 1, 2).squeeze(-1)
-    c = torch.zeros(real.shape, dtype=torch.complex64, device=a.device)
-    c.real, c.imag = real, imag
-
-    return c.view(c.size(0), -1, *c.shape[3:])
+src_path = r"/mnt/data/Datasets/CNAM_CD_d"
 
 
-def to_ntuple(val: Union[int, Iterable[int]], n: int) -> Tuple[int, ...]:
-    """Casts to a tuple with length 'n'.  Useful for automatically computing the
-    padding and stride for convolutions, where users may only provide an integer.
+class CDReader(data.Dataset):
+    def __init__(self,path_root=src_path,mode="train", en_edge=False):
+        super(CDReader,self).__init__()
+        self.en_edge = en_edge
 
-    Args:
-        val: (Union[int, Iterable[int]]) Value to cast into a tuple.
-        n: (int) Desired length of the tuple
+        self.data_list = self._get_list(os.path.join(path_root, mode))
+        self.data_num = len(self.data_list)
 
-    Returns:
-        (Tuple[int, ...]) Tuple of length 'n'
-    """
-    if isinstance(val, Iterable):
-        out = tuple(val)
-        if len(out) == n:
-            return out
+        self.label_info = pd.read_csv(os.path.join(path_root, 'label_info.csv'))
+        self.label_color = np.array([[1,0],[0,1]])
+
+        self.path_root = os.path.join(path_root, mode)
+
+        self.sst1_images = []
+        self.sst1_edge = []
+        self.sst2_images = []
+        self.sst2_edge = []
+        self.gt_images = []
+        
+        if self.en_edge:
+            for _file in self.data_list:
+                self.sst1_images.append(os.path.join(self.path_root, "A", _file))
+                self.sst2_images.append(os.path.join(self.path_root, "B", _file))
+                self.sst1_edge.append(os.path.join(self.path_root, "AEdge", _file))
+                self.sst2_edge.append(os.path.join(self.path_root, "BEdge", _file))
+                self.gt_images.append(os.path.join(self.path_root, "label", _file))
         else:
-            raise ValueError(f"Cannot cast tuple of length {len(out)} to length {n}.")
-    else:
-        return n * (val,)
+            for _file in self.data_list:
+                self.sst1_images.append(os.path.join(self.path_root, "A", _file))
+                self.sst2_images.append(os.path.join(self.path_root, "B", _file))
+                self.gt_images.append(os.path.join(self.path_root, "label", _file))
 
+    def __getitem__(self, index):
 
-def fft_conv(
-    signal: Tensor,
-    kernel: Tensor,
-    bias: Tensor = None,
-    padding: Union[int, Iterable[int], str] = 0,
-    padding_mode: str = "constant",
-    stride: Union[int, Iterable[int]] = 1,
-    dilation: Union[int, Iterable[int]] = 1,
-    groups: int = 1,
-) -> Tensor:
-    """Performs N-d convolution of Tensors using a fast fourier transform, which
-    is very fast for large kernel sizes. Also, optionally adds a bias Tensor after
-    the convolution (in order ot mimic the PyTorch direct convolution).
+        A_path = self.sst1_images[index]
+        B_path = self.sst2_images[index]
+        lab_path = self.gt_images[index]
 
-    Args:
-        signal: (Tensor) Input tensor to be convolved with the kernel.
-        kernel: (Tensor) Convolution kernel.
-        bias: (Tensor) Bias tensor to add to the output.
-        padding: (Union[int, Iterable[int], str) If int, Number of zero samples to pad then
-            input on the last dimension. If str, "same" supported to pad input for size preservation.
-        padding_mode: (str) Padding mode to use from {constant, reflection, replication}.
-                      reflection not available for 3d.
-        stride: (Union[int, Iterable[int]) Stride size for computing output values.
-        dilation: (Union[int, Iterable[int]) Dilation rate for the kernel.
-        groups: (int) Number of groups for the convolution.
+        A_img = self._normalize(np.array(Image.open(A_path)))
+        B_img = self._normalize(np.array(Image.open(B_path)))
 
-    Returns:
-        (Tensor) Convolved tensor
-    """
+        if self.en_edge:
+            AEdge_path = self.sst1_edge[index]
+            BEdge_path = self.sst2_edge[index]
+            edge1 = cv2.imread(AEdge_path, cv2.IMREAD_UNCHANGED)
+            edge2 = cv2.imread(BEdge_path, cv2.IMREAD_UNCHANGED)
+            A_img = np.concatenate((A_img, edge1[..., np.newaxis]), axis=-1)  # 将两个时段的数据concat在通道层
+            B_img = np.concatenate((B_img, edge2[..., np.newaxis]), axis=-1)  # 将两个时段的数据concat在通道层
+        # w, h, _ = A_img.shape
 
-    # Cast padding, stride & dilation to tuples.
-    n = signal.ndim - 2
-    stride_ = to_ntuple(stride, n=n)
-    dilation_ = to_ntuple(dilation, n=n)
-    if isinstance(padding, str):
-        if padding == "same":
-            if stride != 1 or dilation != 1:
-                raise ValueError("stride must be 1 for padding='same'.")
-            padding_ = [(k - 1) / 2 for k in kernel.shape[2:]]
+        sst1 = tfs.ToTensor()(A_img)
+        sst2 = tfs.ToTensor()(B_img)
+
+        gt = np.array(Image.open(lab_path))
+        if (len(gt.shape) == 3):
+            gt = one_hot_it(gt, self.label_info)
+        #gt = np.argmax(gt,axis=2)
         else:
-            raise ValueError(f"Padding mode {padding} not supported.")
-    else:
-        padding_ = to_ntuple(padding, n=n)
+            gt = np.array((gt != 0),dtype=np.int8)
+            gt = self.label_color[gt]
+        gt = np.transpose(gt, [2, 0, 1])
+        gt = torch.from_numpy(gt).type(torch.float32)
+        return sst1, sst2, gt
 
-    # internal dilation offsets
-    offset = torch.zeros(1, 1, *dilation_, device=signal.device, dtype=signal.dtype)
-    offset[(slice(None), slice(None), *((0,) * n))] = 1.0
+    def __len__(self):
+        return self.data_num
 
-    # correct the kernel by cutting off unwanted dilation trailing zeros
-    cutoff = tuple(slice(None, -d + 1 if d != 1 else None) for d in dilation_)
+    def _get_list(self, list_path):
+        
+        # data_list = None
+        
+        # with open(list_path, 'r') as f:
+        #     data_list = f.read().split('\n')[:-1]
+        # return data_list
+        data_list = os.listdir(os.path.join(list_path,'A'))
+        return data_list
 
-    # pad the kernel internally according to the dilation parameters
-    kernel = torch.kron(kernel, offset)[(slice(None), slice(None)) + cutoff]
+    def handle_image(self,img):
+        img=np.array(img)
+        img=Image.fromarray(img).convert("RGB")
+        return self.to_tensor(img).type(torch.float32)
 
-    # Pad the input signal & kernel tensors (round to support even sized convolutions)
-    signal_padding = [r(p) for p in padding_[::-1] for r in (floor, ceil)]
-    signal = f.pad(signal, signal_padding, mode=padding_mode)
-
-    # Because PyTorch computes a *one-sided* FFT, we need the final dimension to
-    # have *even* length.  Just pad with one more zero if the final dimension is odd.
-    signal_size = signal.size()  # original signal size without padding to even
-    if signal.size(-1) % 2 != 0:
-        signal = f.pad(signal, [0, 1])
-
-    kernel_padding = [
-        pad
-        for i in reversed(range(2, signal.ndim))
-        for pad in [0, signal.size(i) - kernel.size(i)]
-    ]
-    padded_kernel = f.pad(kernel, kernel_padding)
-
-    # Perform fourier convolution -- FFT, matrix multiply, then IFFT
-    signal_fr = rfftn(signal.float(), dim=tuple(range(2, signal.ndim)))
-    kernel_fr = rfftn(padded_kernel.float(), dim=tuple(range(2, signal.ndim)))
-
-    kernel_fr.imag *= -1
-    output_fr = complex_matmul(signal_fr, kernel_fr, groups=groups)
-    output = irfftn(output_fr, dim=tuple(range(2, signal.ndim)))
-
-    # Remove extra padded values
-    crop_slices = [slice(None), slice(None)] + [
-        slice(0, (signal_size[i] - kernel.size(i) + 1), stride_[i - 2])
-        for i in range(2, signal.ndim)
-    ]
-    output = output[crop_slices].contiguous()
-
-    # Optionally, add a bias term before returning.
-    if bias is not None:
-        bias_shape = tuple([1, -1] + (signal.ndim - 2) * [1])
-        output += bias.view(bias_shape)
-
-    return output
+    def handle_label(self,label):
+        label=np.array(label)
+        label=one_hot_it(label,self.label_info).astype(np.uint8)
+        label=np.transpose(label,[2,0,1])
+        label=torch.from_numpy(label).type(torch.float32)
+        return label
 
 
-class _FFTConv(nn.Module):
-    """Base class for PyTorch FFT convolution layers."""
+    def randomRotation(image, label, mode=Image.BICUBIC):
 
-    def __init__(
-        self,
-        in_channels: int,
-        out_channels: int,
-        kernel_size: Union[int, Iterable[int]],
-        padding: Union[int, Iterable[int]] = 0,
-        padding_mode: str = "constant",
-        stride: Union[int, Iterable[int]] = 1,
-        dilation: Union[int, Iterable[int]] = 1,
-        groups: int = 1,
-        bias: bool = True,
-        ndim: int = 1,
-    ):
         """
-        Args:
-            in_channels: (int) Number of channels in input tensors
-            out_channels: (int) Number of channels in output tensors
-            kernel_size: (Union[int, Iterable[int]) Square radius of the kernel
-            padding: (Union[int, Iterable[int]) Number of zero samples to pad the
-                input on the last dimension. If str, "same" supported to pad input for size preservation.
-            padding_mode: (str) Padding mode to use from {constant, reflection, replication}.
-                          reflection not available for 3d.
-            stride: (Union[int, Iterable[int]) Stride size for computing output values.
-            dilation: (Union[int, Iterable[int]) Dilation rate for the kernel.
-            groups: (int) Number of groups for the convolution.
-            bias: (bool) If True, includes bias, which is added after convolution
-            ndim: (int) Number of dimensions of the input tensor.
+         对图像进行随机任意角度(0~360度)旋转
+        :param mode 邻近插值,双线性插值,双三次B样条插值(default)
+        :param image PIL的图像image
+        :return: 旋转转之后的图像
         """
-        super().__init__()
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.kernel_size = kernel_size
-        self.padding = padding
-        self.padding_mode = padding_mode
-        self.stride = stride
-        self.dilation = dilation
-        self.groups = groups
-        self.use_bias = bias
+        random_angle = np.random.randint(1, 360)
+        return image.rotate(random_angle, mode), label.rotate(random_angle, Image.NEAREST)
 
-        if in_channels % groups != 0:
-            raise ValueError(
-                "'in_channels' must be divisible by 'groups'."
-                f"Found: in_channels={in_channels}, groups={groups}."
-            )
-        if out_channels % groups != 0:
-            raise ValueError(
-                "'out_channels' must be divisible by 'groups'."
-                f"Found: out_channels={out_channels}, groups={groups}."
-            )
+    def randomCrop(image, label):
+        """
+        对图像随意剪切,考虑到图像大小范围(68,68),使用一个一个大于(36*36)的窗口进行截图
+        :param image: PIL的图像image
+        :return: 剪切之后的图像
+        """
+        image_width = image.size[0]
+        image_height = image.size[1]
+        crop_win_size = np.random.randint(40, 68)
+        random_region = (
+            (image_width - crop_win_size) >> 1, (image_height - crop_win_size) >> 1, (image_width + crop_win_size) >> 1,
+            (image_height + crop_win_size) >> 1)
+        return image.crop(random_region), label
 
-        kernel_size = to_ntuple(kernel_size, ndim)
-        weight = torch.randn(out_channels, in_channels // groups, *kernel_size)
+    @staticmethod
+    def randomGaussian(image, label, mean=0.2, sigma=0.3):
+        """
+         对图像进行高斯噪声处理
+        :param image:
+        :return:
+        """
 
-        self.weight = nn.Parameter(weight)
-        self.bias = nn.Parameter(torch.randn(out_channels)) if bias else None
+        def gaussianNoisy(im, mean=0.2, sigma=0.3):
+            """
+            对图像做高斯噪音处理
+            :param im: 单通道图像
+            :param mean: 偏移量
+            :param sigma: 标准差
+            :return:
+            """
+            for _i in range(len(im)):
+                im[_i] += random.gauss(mean, sigma)
+            return im
+        # 将图像转化成数组
+        img = np.asarray(image)
+        img.flags.writeable = True  # 将数组改为读写模式
+        width, height = img.shape[:2]
+        img_r = gaussianNoisy(img[:, :, 0].flatten(), mean, sigma)
+        img_g = gaussianNoisy(img[:, :, 1].flatten(), mean, sigma)
+        img_b = gaussianNoisy(img[:, :, 2].flatten(), mean, sigma)
+        img[:, :, 0] = img_r.reshape([width, height])
+        img[:, :, 1] = img_g.reshape([width, height])
+        img[:, :, 2] = img_b.reshape([width, height])
+        return Image.fromarray(np.uint8(img)), label
+    
+    @staticmethod
+    def _normalize(img, mean=[0.485, 0.456, 0.406], std=[1, 1, 1]):
+        # return img
+        im = img.astype(np.float32, copy=False)
+        mean = np.array(mean)[np.newaxis, np.newaxis, :]
+        std = np.array(std)[np.newaxis, np.newaxis, :]
+        im = im / 255.0
+        im -= mean
+        im /= std
+        return im
 
-    def forward(self, signal):
-        return fft_conv(
-            signal,
-            self.weight,
-            bias=self.bias,
-            padding=self.padding,
-            padding_mode=self.padding_mode,
-            stride=self.stride,
-            dilation=self.dilation,
-            groups=self.groups,
-        )
 
 
-FFTConv1d = partial(_FFTConv, ndim=1)
-FFTConv2d = partial(_FFTConv, ndim=2)
-FFTConv3d = partial(_FFTConv, ndim=3)
+class TestReader(CDReader):
+    def __init__(self, path_root, mode, en_edge=False):
+        super(TestReader, self).__init__(path_root, mode, en_edge)
+
+        self.data_name = os.path.split(path_root)[-1]
+
+        self.file_name = []
+        if self.en_edge:
+            for _file in self.data_list:
+                self.sst1_images.append(os.path.join(f"{self.path_root}/A", _file))
+                self.sst2_images.append(os.path.join(f"{self.path_root}/B", _file))
+                self.sst1_edge.append(os.path.join(f"{self.path_root}/AEdge", _file))
+                self.sst2_edge.append(os.path.join(f"{self.path_root}/BEdge", _file))
+                self.gt_images.append(os.path.join(f"{self.path_root}/label", _file))
+                self.file_name.append(_file)
+        else:
+            for _file in self.data_list:
+                self.sst1_images.append(os.path.join(f"{self.path_root}/A", _file))
+                self.sst2_images.append(os.path.join(f"{self.path_root}/B", _file))
+                self.gt_images.append(os.path.join(f"{self.path_root}/label", _file))
+                self.file_name.append(_file)
+
+
+    def __getitem__(self, index):
+        # print(self.data_list[index])
+        A_path = self.sst1_images[index]
+        B_path = self.sst2_images[index]
+        lab_path = self.gt_images[index]
+
+        A_img = self._normalize(np.array(Image.open(A_path)))
+        B_img = self._normalize(np.array(Image.open(B_path)))
+
+        if self.en_edge:
+            AEdge_path = self.sst1_edge[index]
+            BEdge_path = self.sst2_edge[index]
+            edge1 = cv2.imread(AEdge_path, cv2.IMREAD_UNCHANGED)
+            edge2 = cv2.imread(BEdge_path, cv2.IMREAD_UNCHANGED)
+            A_img = np.concatenate((A_img, edge1[..., np.newaxis]), axis=-1)  # 将两个时段的数据concat在通道层
+            B_img = np.concatenate((B_img, edge2[..., np.newaxis]), axis=-1)  # 将两个时段的数据concat在通道层
+        # w, h, _ = A_img.shape
+
+        sst1 = tfs.ToTensor()(A_img)
+        sst2 = tfs.ToTensor()(B_img)
+
+        gt = np.array(Image.open(lab_path))
+        if (len(gt.shape) == 3):
+            gt = one_hot_it(gt, self.label_info)
+        #gt = np.argmax(gt,axis=2)
+        else:
+            gt = np.array((gt != 0),dtype=np.int8)
+            gt = self.label_color[gt]
+            
+        gt = np.transpose(gt, [2, 0, 1])
+        gt = torch.from_numpy(gt).type(torch.float32)
+
+        return sst1, sst2, gt, self.file_name[index]
+
+    def __len__(self):
+        return self.data_num
+
+
+def detect_building_edge(data_path, save_pic_path):
+    canny_low = 180
+    canny_high = 210
+    hough_threshold = 64
+    hough_minLineLength = 16
+    hough_maxLineGap = 3
+    hough_rho = 1
+    hough_theta = np.pi / 180
+    image_names=os.listdir(data_path)
+    for image_name in image_names:
+        img=cv2.imread(os.path.join(data_path, image_name))
+        shape=img.shape[:2]
+        img_gray=cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+        edges=cv2.Canny(img_gray,canny_low,canny_high)
+        lines=cv2.HoughLinesP(edges,hough_rho,hough_theta,hough_threshold,hough_minLineLength,hough_maxLineGap)
+        line_pic = np.zeros(shape, np.uint8)
+        if lines is not None:
+            for line in lines:
+                x1, y1, x2, y2 = line[0]
+                cv2.line(line_pic, (x1, y1), (x2, y2), 1, thickness=1)
+        cv2.imwrite(os.path.join(save_pic_path, image_name),line_pic)
+
 
 if __name__ == "__main__":
-    print("test fftconv")
-    x = torch.rand([2,4,256,256]).cuda()
-    m = FFTConv2d(4,2,3,1).to("cuda")
-    y = m(x)
-    print(y.shape)
+    dataset_path = '/mnt/data/Datasets/CLCD'
+    x = np.random.random([4,4,3])
+    mean = np.std(x, axis=(0,1))
+    mean = np.array(mean)[np.newaxis, np.newaxis, :]
+    print(x)
+    print(mean)
+
+    
